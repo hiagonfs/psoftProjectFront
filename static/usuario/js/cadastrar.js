ab1bcb36bcc36bbdc15d940e7194cb5ff495729a
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2019-11-09 21:02:44 -0300

    implementando UC1

---
+++ static/usuario/js/cadastrar.js
@@ -0,0 +1,25 @@
+let URL = 'https://http://localhost:8080/usuario';
+let $botaoCadastrar = document.querySelector("#criar");
+$botaoCadastrar.addEventListener('click', cadastrar_usuario);
+
+function cadastrar_usuario() {
+  let nome = document.querySelector("#nome").value;
+  let sobrenome = document.querySelector("#sobrenome").value;
+  let email = document.querySelector("#email").value;
+  let numCartaoCredito = document.querySelector("#numCartaoCredito").value;
+  let senha = document.querySelector("#senha").value;
+  fetch(URL, {
+    'method': 'POST',
+    'body': `{"nome": "${nome}", "sobrenome": "${sobrenome}",
+              "email": "${email}", "numCartaoCredito": "${numCartaoCredito}",
+              "senha": "${senha}"}`,
+    'headers': {'Content-Type': 'application/json'}
+  })
+  .then(r => r.json())
+  .then(u => {
+      console.log(u);
+      console.log('Pronto! Usuario criado com sucesso!');
+      usuarios.push(u);
+      fetch_usuarios();
+  });
+}
