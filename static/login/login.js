a8b1dbc3707f21059f712806104a7bc13cd514e3
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2019-11-14 20:41:37 -0300

    login funcionando

--- static/login/login.js
+++ static/login/login.js
@@ -2,39 +2,38 @@ let username;
 let $viewer = document.querySelector('#viewer');
 
 (async function main() {
-  
+
     // roteamento
     let hash = location.hash;
     if (["", "#viewLogin"].includes(hash)) {
       viewLogin();
+    }
 
-    } 
-    
     else if (["#viewCadastroUsuario"].includes(hash)){
-        viewCadastroUsuario(); 
-    } 
-    
+        viewCadastroUsuario();
+    }
+
     else if (["#viewHome"].includes(hash)) {
       viewHome();
-    } 
-    
+    }
+
     else if(["viewCadastraCampanha"].includes(hash)) {
-        viewCadastraCampanha(); 
+        viewCadastraCampanha();
     }
 
   }());
 
 function viewLogin() {
-    
+
     let $template = document.querySelector('#viewLogin');
     $viewer.innerHTML = $template.innerHTML;
 
     let $botaoLogar = document.querySelector("#logar");
     let $botaoCadastrar = document.querySelector("#cadastrar");
 
-    $botaoLogar.addEventListener('click', viewCadastraCampanha);
+    $botaoLogar.addEventListener('click', logar);
     $botaoCadastrar.addEventListener('click', viewCadastroUsuario);
-  
+
 }
 
 function viewCadastroUsuario () {
@@ -55,26 +54,31 @@ function viewHome() {
 
 }
 
+function cadastrar_usuario() {
+  
+}
+
 function logar() {
 
-    let URL = 'http://localhost:8080/login';
+    let URL = 'http://localhost:8080/auth/login';
 
-    // let email = document.querySelector("#email").value;
-    // let senha = document.querySelector("#senha").value;
+     let email = document.querySelector("#email").value;
+     let senha = document.querySelector("#senha").value;
 
-    // fetch(URL, {
-    //     'method': 'POST',
-    //     'body': `{"email": "${email}", 
-    //               "senha": "${senha}"`,
-    //     'headers': {'Content-Type': 'application/json'}
-    //   })
-    //   .then(r => r.json()) 
-    //   .then(u => {
-
-    //       console.log('Pronto! Logando no sistema!');
-    //       roteiaPaginaHome(); 
-          
-    //   });
+     fetch(URL, {
+         'method': 'POST',
+         'body': `{"email": "${email}",
+                   "senha": "${senha}"}`,
+         'headers': {'Content-Type': 'application/json'}
+       })
+       .then(r => r.json())
+       .then(u => {
+           console.log(email);
+           console.log(senha);
+           console.log(u);
+           console.log('Pronto! Logando no sistema!');
+           viewCadastraCampanha();
+       });
 
 }
 
