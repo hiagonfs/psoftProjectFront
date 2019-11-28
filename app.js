49bdb8ecb52daa8d0f2dd54d81b89384fda9abc9
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2019-11-28 09:21:19 -0300

    solucionando problema de concorrencia top5

--- app.js
+++ app.js
@@ -59,6 +59,15 @@ function viewPrincipal() {
   listaTop5Campanhas();
 }
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 async function listaTop5Campanhas() {
 
   criterioDeOrdenacao = document.querySelector("#metodoOrdenacao").value;
@@ -86,6 +95,7 @@ async function listaTop5Campanhas() {
 
       campanhas.push(json[i]);
       buscaQuantoFaltaMeta(json[i], i, $divParaCampanhas);
+      sleep(15);
     });
 //  })();
 
